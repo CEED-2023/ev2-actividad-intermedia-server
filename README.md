1f7ce4b73584d7116c77cac5bc41822f02375a4e
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-11-17 14:06:38 +0100

    Working on db data

--- README.md
+++ README.md
@@ -3,4 +3,36 @@
 
 https://fastify.dev/docs/latest/Guides/Getting-Started
 
+https://github.com/punkish/fastify-better-sqlite3
 
+
+WITH RECURSIVE DepartmentHierarchy AS (
+  SELECT
+    id,
+    parent_id,
+    name,
+    1 as depth
+  FROM
+    departments
+  WHERE
+    parent_id IS NULL
+
+  UNION ALL
+
+  SELECT
+    d.id,
+    d.parent_id,
+    d.name,
+    dh.depth + 1
+  FROM
+    departments d
+  JOIN
+    DepartmentHierarchy dh ON d.parent_id = dh.id
+)
+
+SELECT
+  RPAD('', depth - 1, '  ') || name AS department_tree
+FROM
+  DepartmentHierarchy
+ORDER BY
+  depth, name;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 // Import the framework and instantiate it
 import Fastify from 'fastify'
+
+import fastifyBetterSqlite3 from '@punkish/fastify-better-sqlite3';
 import firstRoute from './our-first-route.js'
 
 const fastify = Fastify({
   logger: true
 })
 
-fastify.register(firstRoute)
-
 // Run the server!
 try {
+
+  const opts1 = {};
+
+  const opts2 = {
+
+    // if db file doesn't exist, it will be created unless
+    // `betterSqlite3options.fileMustExist: true`
+    path: '/path/to/db.sqlite',
+
+    // The following options are passed on to `better-sqlite3`.
+    // The default values are shown below, and none are required.
+    // Check better-sqlite3 documentation for details.
+    betterSqlite3options: {
+        readonly: false,
+        fileMustExist: false,
+        timeout: 5000,
+        verbose: null
+    }
+  };
+
+  fastify.register(fastifyBetterSqlite3, opts1);
+  fastify.register(firstRoute)
+
   await fastify.listen({ port: 3000, host: '0.0.0.0' })
 } catch (err) {
   fastify.log.error(err)
