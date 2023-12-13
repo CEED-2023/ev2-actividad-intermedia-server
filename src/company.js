85bd0e6e13765211dc76768b154089e43f13fcf7
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-12-13 10:07:54 +0100

    Company API ready

--- src/company.js
+++ src/company.js
@@ -1,33 +1,52 @@
 import { generateCompany } from './generators/company_generator.js'
 
+function departmentId(department) {
+  return department.id
+}
+
+const COMPANY_REQUEST_SCHEMA = {
+  querystring: {
+    type: 'object',
+    properties: {
+      id: {
+        type: 'integer'
+      }
+    },
+    required: ['id']
+  }
+}
+
 async function routes(fastify, _options) {
 
-  async function handler(request, _reply) {
+  async function companyData_handler(request, _reply) {
     const id = request.query.id
     let company = generateCompany(id)
 
-    //TO-DO: return only company data and first department
-    return company
+    return {
+      id: company.id,
+      name: company.company_name,
+      departments: company.departments.map(departmentId)
+    }
   }
 
   ['/', '/company'].forEach(path => {
     fastify.route({
       method: ['GET'], // you could define multiple methods
       url: path,
-      schema: {
-        querystring: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'integer'
-            }
-          },
-          required: ['id']
-        }
-      },
-      handler: handler
+      schema: COMPANY_REQUEST_SCHEMA,
+      handler: companyData_handler
     })
   })
+
+  // Get the full company for debuggin purposes
+  fastify.get(
+    '/company/full',
+    { schema: COMPANY_REQUEST_SCHEMA },
+    async (request, _reply) => {
+      const id = request.query.id
+      return generateCompany(id)
+    }
+  )
 }
 
 
