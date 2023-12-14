64ae6f9502e3c21b4c5d59324d6affca53263f42
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-12-14 10:41:55 +0100

    Adds swagger documentation

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ https://github.com/punkish/fastify-better-sqlite3
 
 PARA LA DOCUMENTACION:
 https://ruanmartinelli.com/posts/using-schemas-fastify-fun-and-profit/
+https://stackoverflow.com/questions/76168789/how-to-setup-fastify-swagger-and-fastify-swagger-ui-with-typescript
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // Import the framework and instantiate it
 import Fastify from 'fastify'
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
 import company from './company.js'
 import department from './department.js'
 
@@ -9,6 +12,29 @@ const PORT = process.env.PORT || DEFAULT_PORT // Heroku assigns you a port
 const fastify = Fastify({
   logger: true
 })
+
+const swaggerOptions = {
+  swagger: {
+      info: {
+          title: "Wally Enterprises API",
+          description: "API con compañías para las que ha trabajado Wally",
+          version: "1.0.0",
+      },
+      host: "localhost",
+      schemes: ["http", "https"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      tags: [{ name: "Default", description: "Default" }],
+  },
+};
+
+const swaggerUiOptions = {
+  routePrefix: "/docs",
+  exposeRoute: true,
+};
+
+fastify.register(fastifySwagger, swaggerOptions);
+fastify.register(fastifySwaggerUi, swaggerUiOptions);
 
 // Run the server!
 try {
